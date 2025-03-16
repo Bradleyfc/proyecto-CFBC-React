4961d63639e135fe998d04b7347136c542f56777
Bradleyfc <bradleyfdez@gmail.com>
2025-03-16 12:50:56 -0400

    otro

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/proyeccto-CFBC-React/',
+  base: '/proyecto-CFBC-React/',
   plugins: [react()],
 })
